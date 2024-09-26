b97f22caa1dbe77f242449a328a9cf56ba4cdd31
Rishabh1Mishra <rishabh6mishra@gmail.com>
2024-09-26 20:43:19 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{ 
+    int a =5;
+    int b =6;
+    int c;
+    c=a;
+    b =a;
+   
+    
+    
+    
+    printf("%d",c);
+
+    return 0;
+}
